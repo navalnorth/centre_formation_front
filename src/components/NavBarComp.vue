9529fd19004ba6f694406b37eecfc55096679cd6
yamine59 <yamineizi22@gmail.com>
2024-11-28 17:12:47 +0100

    feat: accueil et responsible

--- src/components/NavBarComp.vue
+++ src/components/NavBarComp.vue
@@ -4,10 +4,10 @@
             <img class="w-16 h-16" :src="logo" alt="Logo" />
         </div>
         <div class="hidden  w-1/2 min-w-96 justify-between md:flex text-white fontTitle">
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/monProfil">Qui suis-je ?</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/Decouvrirprofil">Bilan de compétences</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/matchs">Formation</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/concept">Contactez-moi</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" to="/">Qui suis-je ?</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" to="/">Bilan de compétences</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" to="/">Formation</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" to="/">Contactez-moi</router-link>
         </div>
         <div class="hidden md:flex">
             <p class="bg-white p-2  rounded-xl hover:bg-red-100 fontTitle ">Prendre rendez-vous</p>
@@ -15,11 +15,12 @@
         <div @click="toggleBurgerMenu" class="flex md:hidden" >
             <ChMenuHamburger class="w-14 h-14 text-white"/>
         </div>
-        <div :class="isOpen ? 'menu closed' : 'menu open' " >
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/monProfil">Qui suis-je ?</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/Decouvrirprofil">Bilan de compétences</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/matchs">Formation</router-link>
-            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/concept">Contactez-moi</router-link>
+        <div :class="isOpen ? 'menu closed' : 'menu open' " class="z-40 p-10" >
+            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/">Qui suis-je ?</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/">Bilan de compétences</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/">Formation</router-link>
+            <router-link class="cursor-pointer hover:text-slate-400" @click="toggleBurgerMenu" to="/">Contactez-moi</router-link>
+            <p class="bg-pink-300 text-white p-2 w-80  rounded-xl hover:bg-red-100 fontTitle"  @click="toggleBurgerMenu" to="/">Prendre rendez-vous</p>
         </div>
     </nav>
 </template>
@@ -44,10 +45,11 @@ const logo = logoPath;
     display: flex;
     flex-direction: column;
     position: absolute;
+    align-items: center;
     background-color: aliceblue;
     width: 100vw;
     margin: 0;
-    padding-top: 5px;
+    padding-top: 25px;
     left: 0;
     right: 0;
     top: 96px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="home">
+  <BannierAccueilComp :image="image">
+    <template #title>FORMATIONS &  BILAN DE COMPETENCES </template>
+    <template #name>PAULINE BOURDARIAS</template>
+    <template #description>Parce que chaque parcours est unique, Je vous accompagne avec bienveillance et écoute pour révéler
+      votre 
+      potentiel
+      et donner du sens à vos projets. Ensemble, construisons un avenir qui vous ressemble. </template>
+  </BannierAccueilComp>
+  <div class="flex flex-col items-center justify-center text-white ">
     
-    <h1 class="text-8xl">test</h1>
   </div>
+ 
 </template>
 
 <script setup>
+import BannierAccueilComp from '@/components/BannierAccueilComp.vue';
 
-
+import image from '../assets/logo/logo_3.png'
 </script>
+
+<style scoped>
+
+</style>
