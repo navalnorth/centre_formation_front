c5534eedf75581cd6f9b6af5fc7fec814b0606c0
yamine59 <yamineizi22@gmail.com>
2024-11-28 17:27:56 +0100

    fix: bannier size

--- src/components/BannierAccueilComp.vue
+++ src/components/BannierAccueilComp.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="flex flex-col items-center justify-center text-white">
         <!-- Div avec dégradé dynamique et coin arrondi -->
-        <div class="background-div  "></div>
-
-        <div class="flex items-center justify-center flex-wrap-reverse gap-10 md:gap-44 w-full mt-32 mx-0 md:mx-10 relative z-10">
+        <div class="background-div  ">
+             <div class="flex items-center justify-center flex-wrap-reverse gap-10 md:gap-44 w-full mt-32 mx-0 md:mx-10 relative z-10">
             <div class="text-center md:text-left md:w-1/3 w-full">
-                <h1 class="text-xl md:text-3xl  font-bold fontTitle px-5"><slot name="title"></slot></h1>
+                <h1 class="text-xl md:text-3xl  font-bold fontTitle md:p-0 px-5"><slot name="title"></slot></h1>
                 <h3 class="text-base md:text-xl mt-2 font-light"><slot name="name"></slot></h3>
                 <p class="mt-4 text-base sp fontSubTitle w-full break-words md:p-0 px-5"><slot name="description"></slot></p>
             </div>
@@ -14,6 +13,9 @@
                 <p class="p-2 bg-white text-black w-56 rounded-xl text-center">Qui suis-je ?</p>
             </div>
         </div>
+        </div>
+
+       
     </div>
 </template>
 
@@ -46,7 +48,8 @@ defineProps({
   top: 0;
   left: 0;
   width: 100%;
-  height: 66.666667%;
+  height: auto;
+  padding-bottom: 100px;
   background: linear-gradient(to right, var(--primary-color), var(--secondary-color));
   /* Forme avec un coin arrondi en bas à droite */
   border-bottom-right-radius: 50%; /* Arrondi du coin inférieur droit */
