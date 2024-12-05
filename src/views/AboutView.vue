8e6ad104b3b5246e33aa16d53041286254c1f927
unknown <moha_naval@live.com>
2024-12-05 13:54:17 +0100

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,11 +4,29 @@
             {{ bannierHautTitle }}
         </template>
     </BannierPagesComp>
-    <div class="px-24 py-28">
-        <div class="w-2/3">
-            <p class="text-left">
-                {{texte}}
+    <div class="dispo p-28 flex justify-center items-center gap-40">
+        <div class="left text-left">
+            <p>
+                <strong>{{titre_description}}</strong><br><br>
             </p>
+            <p>
+                {{description1}}
+            </p><br>
+            <p>
+                {{description2}}
+            </p><br>
+            <p>
+                {{description3}}
+            </p>
+        </div>
+
+        <div class="right flex flex-col gap-10 self-start">
+            <div>
+                <img :src="`${url}${image}`" alt="Formation" class="rounded-3xl">
+            </div>
+            <div>
+                <img src="../assets/image/Avion.png" alt="Bilan-de-competences">
+            </div>
         </div>
     </div>
 </template>
@@ -18,18 +36,18 @@
 import BannierPagesComp from '@/components/BannierPagesComp.vue';
 import { onMounted, ref } from 'vue';
 
+const url = `${process.env.VUE_APP_URL}/uploads/`;
+
 const bannierHautTitle = ref('Qui suis-je ?')
-const texte = ref(`Passionnée par l'humain et les parcours de vie, mon chemin a été tout sauf linéaire !
-                    Forte d'une formation en ressources humaines et d'expériences variées, allant du commerce à la gestion d'un restaurant, en passant par des missions en recrutement et en RH, j'ai toujours cherché à créer du lien et à révéler le potentiel de chacun. 
-                    Originaire de la région parisienne, c'est l'amour (et des yeux bleus !) qui m'a menée dans le Nord, où j'ai découvert ma vocation : l'accompagnement.
-                    Depuis, j'ai eu la chance de guider des salariés, des demandeurs d'emploi, des bénéficiaires du RSA, et bien d'autres profils dans des contextes variés : plans sociaux, réinsertion professionnelle, et bien plus encore.
-                    Aujourd'hui, je mets cette richesse d'expériences au service de votre réussite, que vous soyez une entreprise ou un particulier.
-                    Ensemble, avançons vers vos objectifs avec confiance et authenticité.
-                    Si vous souhaitez personnaliser encore davantage, n'hésitez pas à indiquer vos préférences !`)
+const titre_description = ref(`Passionnée par l'humain et les parcours de vie, mon chemin a été tout sauf linéaire !`)
+const description1 = ref(`Forte d'une formation en ressources humaines et d'expériences variées, allant du commerce à la gestion d'un restaurant, en passant par des missions en recrutement et en RH, j'ai toujours cherché à créer du lien et à révéler le potentiel de chacun. Originaire de la région parisienne, c'est l'amour (et des yeux bleus !) qui m'a menée dans le Nord, où j'ai découvert ma vocation : l'accompagnement.`)
+const description2 = ref(`Depuis, j'ai eu la chance de guider des salariés, des demandeurs d'emploi, des bénéficiaires du RSA, et bien d'autres profils dans des contextes variés : plans sociaux, réinsertion professionnelle, et bien plus encore.`)
+const description3 = ref(`Aujourd'hui, je mets cette richesse d'expériences au service de votre réussite, que vous soyez une entreprise ou un particulier. Ensemble, avançons vers vos objectifs avec confiance et authenticité.  Si vous souhaitez personnaliser encore davantage, n'hésitez pas à indiquer vos préférences !`)
+const image = ref('image_accueil-1733156237736.jpg')
 
 
 
-const fetchBilan = async () => {
+const fechAbout = async () => {
     try {
         const response = await fetch(`${process.env.VUE_APP_URL}/users/`, {
             method: 'GET',
@@ -49,13 +67,48 @@ const fetchBilan = async () => {
 
         const data = result.data[0];
         bannierHautTitle.value = data.title;
-        texte.value = data.description;
+        titre_description.value = data.title_description;
+        description1.value = data.description1;
+        description2.value = data.description2;
+        description3.value = data.description3;
+        image.value = data.image;
+
+        console.log(data);
+        
     } catch (error) {
         console.error('Erreur durant la connexion :', error);
     }
 };
 
 onMounted(async () => {
-    await fetchBilan();
+    await fechAbout();
 });
 </script>
+
+
+<style scoped>
+@media (max-width: 1100px) {
+    .dispo {
+        flex-direction: column;
+        gap: 56px;
+        padding: 48px;
+    }
+    .right {
+        width: 30%;
+        align-self: center;
+    }
+}
+@media (max-width: 690px) {
+    .right {
+        width: 70%;
+    }
+}
+@media (min-width: 1100px) {
+    .right {
+        width: 20%;
+    }
+    .left {
+        width: 40%;
+    }
+}
+</style>
